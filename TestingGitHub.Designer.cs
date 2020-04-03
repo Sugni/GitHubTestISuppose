e6967fe10fedcd029c8bfd28f7129079a3122e3f
Ingus Andris Pērkons <slaimy333@gmail.com>
2020-04-03 17:01:47 +0300

    Lowkey this branch is better because the cooler button is there. It changes color. Thats it.... I know it's not that impressive but still. It's neat i think.

--- TestingGitHub.Designer.cs
+++ TestingGitHub.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GitHubTestISuppose
 {
-    partial class Form1
+    partial class TestingGitHub
     {
         /// <summary>
         /// Required designer variable.
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.butnClickMe = new System.Windows.Forms.Button();
+            this.butnThatIsBetter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // butnClickMe
@@ -41,11 +42,22 @@
             this.butnClickMe.UseVisualStyleBackColor = true;
             this.butnClickMe.Click += new System.EventHandler(this.butnClickMe_Click);
             // 
+            // butnThatIsBetter
+            // 
+            this.butnThatIsBetter.Location = new System.Drawing.Point(100, 12);
+            this.butnThatIsBetter.Name = "butnThatIsBetter";
+            this.butnThatIsBetter.Size = new System.Drawing.Size(206, 124);
+            this.butnThatIsBetter.TabIndex = 1;
+            this.butnThatIsBetter.Text = "The Cooler Button";
+            this.butnThatIsBetter.UseVisualStyleBackColor = true;
+            this.butnThatIsBetter.Click += new System.EventHandler(this.butnThatIsBetter_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butnThatIsBetter);
             this.Controls.Add(this.butnClickMe);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button butnClickMe;
+        private System.Windows.Forms.Button butnThatIsBetter;
     }
 }
 
--- TestingGitHub.cs
+++ TestingGitHub.cs
@@ -10,9 +10,12 @@ using System.Windows.Forms;
 
 namespace GitHubTestISuppose
 {
-    public partial class Form1 : Form
+    public partial class TestingGitHub : Form
     {
-        public Form1()
+
+        Random rand = new Random();
+
+        public TestingGitHub()
         {
             InitializeComponent();
         }
@@ -20,6 +23,11 @@ namespace GitHubTestISuppose
         private void butnClickMe_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Don't mind me. I'm just a button to test adding changes to projects in github.");
+        }
+
+        private void butnThatIsBetter_Click(object sender, EventArgs e)
+        {
+            butnThatIsBetter.BackColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
         }
     }
 }
